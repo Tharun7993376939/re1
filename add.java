465fe74f4832482ac2daa11241c413807c7595cd
sai <sai@gmial.com>
2024-08-27 14:14:51 +0530

    this commited

---
+++ add.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+class add
+{
+    public static void main(string args[])
+    {
+        scanner sc=new Scanner(system.in);
+        system.out.println("enter the number");
+       int a=Integer.parseInt(sc.nextline);
+       system.out.println("enter the number");
+       int b=Integer.parseInt(sc.nextline);
+       system.out.println(a+b)
+    }
+    
+}
